0befb784ec55af930f0827046e2bb111878e2dfd
abhisek <abhisek.datta@gmail.com>
2024-12-05 16:48:28 +0530

    Integrate with malysis

--- ossfeeds/go.mod
+++ ossfeeds/go.mod
@@ -2,10 +2,31 @@ module github.com/safedep/examples/ossfeeds
 
 go 1.23.2
 
-require github.com/ossf/package-feeds v0.0.0-20240903033607-939890176fa6
+require (
+	buf.build/gen/go/safedep/api/grpc/go v1.5.1-20241205081347-29d2e8505797.1
+	github.com/ossf/package-feeds v0.0.0-20240903033607-939890176fa6
+	github.com/safedep/dry v0.0.0-20241205053748-945ecaca69ba
+	google.golang.org/grpc v1.68.0
+)
 
 require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.35.2-20241127180247-a33202765966.1 // indirect
+	buf.build/gen/go/safedep/api/protocolbuffers/go v1.35.2-20241205081347-29d2e8505797.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.57.0 // indirect
+	go.opentelemetry.io/otel v1.32.0 // indirect
+	go.opentelemetry.io/otel/metric v1.32.0 // indirect
+	go.opentelemetry.io/otel/trace v1.32.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
--- ossfeeds/main.go
+++ ossfeeds/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
 	"time"
 
+	"buf.build/gen/go/safedep/api/grpc/go/safedep/services/malysis/v1/malysisv1grpc"
+	malysisv1pb "buf.build/gen/go/safedep/api/protocolbuffers/go/safedep/messages/malysis/v1"
+	packagev1 "buf.build/gen/go/safedep/api/protocolbuffers/go/safedep/messages/package/v1"
+	malysisv1 "buf.build/gen/go/safedep/api/protocolbuffers/go/safedep/services/malysis/v1"
 	"github.com/ossf/package-feeds/pkg/events"
 	"github.com/ossf/package-feeds/pkg/feeds"
 	"github.com/ossf/package-feeds/pkg/feeds/npm"
+	drygrpc "github.com/safedep/dry/adapters/grpc"
+	"google.golang.org/grpc"
 )
 
 type npmEventHandler struct{}
@@ -26,6 +35,24 @@ func main() {
 		panic(err)
 	}
 
+	tok := os.Getenv("SAFEDEP_API_KEY")
+	tenantId := os.Getenv("SAFEDEP_TENANT_ID")
+
+	if tok == "" || tenantId == "" {
+		panic("SAFEDEP_API_KEY and SAFEDEP_TENANT_ID must be set")
+	}
+
+	headers := http.Header{}
+	headers.Set("x-tenant-id", tenantId)
+
+	cc, err := drygrpc.GrpcClient("pkg-feed-client", "api.safedep.io", "443",
+		tok, headers, []grpc.DialOption{})
+	if err != nil {
+		panic(err)
+	}
+
+	service := malysisv1grpc.NewMalwareAnalysisServiceClient(cc)
+
 	cutoff := time.Now().Add(-time.Hour * 1)
 	for {
 		packages, newCutoff, errs := npmFeed.Latest(cutoff)
@@ -37,8 +64,35 @@ func main() {
 		for _, pkg := range packages {
 			fmt.Printf("Type: %s Package: %s, Version: %s SchemaVer: %s\n",
 				pkg.Type, pkg.Name, pkg.Version, pkg.SchemaVer)
+
+			if err := submitForAnalysis(service, pkg); err != nil {
+				fmt.Printf("Error submitting package for analysis: %v\n", err)
+			}
 		}
 
 		cutoff = newCutoff
 	}
+}
+
+func submitForAnalysis(client malysisv1grpc.MalwareAnalysisServiceClient, pkg *feeds.Package) error {
+	req := malysisv1.AnalyzePackageRequest{
+		Target: &malysisv1pb.PackageAnalysisTarget{
+			PackageVersion: &packagev1.PackageVersion{
+				Package: &packagev1.Package{
+					Ecosystem: packagev1.Ecosystem_ECOSYSTEM_NPM,
+					Name:      pkg.Name,
+				},
+				Version: pkg.Version,
+			},
+		},
+	}
+
+	res, err := client.AnalyzePackage(context.Background(), &req)
+	if err != nil {
+		return fmt.Errorf("error submitting package for analysis: %w", err)
+	}
+
+	fmt.Printf("Submitted package for analysis: %s\n", res.GetAnalysisId())
+
+	return nil
 }
